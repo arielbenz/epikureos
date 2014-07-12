7fe190c3639401a4f665572cc3a7cf0210ac7ad0
arielbenz <arielmbenz@gmail.com>
2014-07-12 11:58:57 -0300

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/* Gruntfile.js */
+module.exports = function(grunt) {
+ 
+// Project configuration.
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+ 
+    compass: {
+        dev: {
+            options: {
+               /* Either use your config.rb for settings, or state them here */
+               //config: 'config.rb'
+               // require:['susy'],
+               httpPath:"/",
+               sassDir:"sass",
+               cssDir:"css",
+               imagesDir:"img",
+               // javascriptsDir:"site/assets/js",
+               // fontsDir:"site/assets/fonts",
+               outputStyle:"compressed",
+               noLineComments:true,
+               relativeAssets:true,
+               raw: "preferred_syntax = :sass\n"
+            }
+        }
+    },
+    watch: {
+        styles: {
+            files: ['sass/**/*.scss',],
+            tasks: ['compass'],
+            options: {
+                spawn: false,
+            },
+        },
+    }
+});
+ 
+// Load plugins here
+grunt.loadNpmTasks('grunt-contrib-compass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-devtools');
+ 
+// Default task(s).
+grunt.registerTask('default', ['compass']);
+grunt.registerTask('dev', ['watch']);
+ 
+};
