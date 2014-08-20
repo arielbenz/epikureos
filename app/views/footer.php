ca3f561d7a06ef734d01a1cc1e65075fe08b6abc
Ariel <arielmbenz@gmail.com>
2014-08-20 00:26:17 -0300

    cambios y analytics

--- app/views/footer.php
+++ app/views/footer.php
@@ -67,29 +67,13 @@
 	<script src="<?php echo $url?>/js/bootstrap.min.js"></script>
 	<script src="<?php echo $url?>/js/bootbox.min.js"></script>
 
-	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-47164304-1']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-		if (window.location.hash && window.location.hash === "#_=_") {
-			if (Modernizr.history) {
-		    	window.history.replaceState("", document.title, window.location.pathname);
-		  	} else {
-		    	var scroll = {
-		      		top: document.body.scrollTop,
-		      		left: document.body.scrollLeft
-		    	};
-		    	window.location.hash = "";
-		    	document.body.scrollTop = scroll.top;
-		    	document.body.scrollLeft = scroll.left;
-		  	}
-		}
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-50343708-1', 'auto');
+	  ga('send', 'pageview');
 	</script>
 
 	<script>
